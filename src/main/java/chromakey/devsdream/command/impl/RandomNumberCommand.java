307de7af8ea765df9e706e36c1814c9983f801e2
ChromaKey <williamcipher2006@gmail.com>
2020-07-17 17:40:04 -0400

    /rng now sends chat feedback

--- src/main/java/chromakey/devsdream/command/impl/RandomNumberCommand.java
+++ src/main/java/chromakey/devsdream/command/impl/RandomNumberCommand.java
@@ -8,18 +8,25 @@ import com.mojang.brigadier.tree.LiteralCommandNode;
 
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class RandomNumberCommand {
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
         LiteralCommandNode<CommandSource> literalCommandNode = dispatcher.register(Commands.literal("random").requires((user) -> {
             return user.hasPermissionLevel(2);
-        }).then(Commands.argument("maximum", IntegerArgumentType.integer(0, 2147483647)).executes((integer) -> {
-            Random rand = new Random();
-            return rand.nextInt(IntegerArgumentType.getInteger(integer, "maximum"));
+        }).then(Commands.argument("maximum", IntegerArgumentType.integer(0, 2147483647)).executes((rng) -> {
+            return rng(rng.getSource(), IntegerArgumentType.getInteger(rng, "maximum"));
         })));
         
         dispatcher.register(Commands.literal("rng").requires((user) -> {
             return user.hasPermissionLevel(2);
         }).redirect(literalCommandNode));
+    }
+
+    private static int rng(CommandSource source, int maximum) {
+        Random rand = new Random();
+        int result = rand.nextInt(maximum);
+        source.sendFeedback(new TranslationTextComponent("commands.devsdream.rng.result", result), true);
+        return result;
     }
 }
